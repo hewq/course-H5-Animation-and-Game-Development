e40a417101b6f5aeb0fb3e3241bdf30914444cdd
wenqiu.he <wenqiu.he@yahoo.com>
2018-08-04 16:53:30 +0800

    add demo4

---
+++ ch07/LS07/Demo4/src/resource.js
@@ -0,0 +1,14 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+
+    // menu
+    MusicOnNormal_png : "res/menu/music-off-normal.png",
+    MusicOnSelected_png : "res/menu/music-off-selected.png",
+    MusicOffNormal_png : "res/menu/music-on-normal.png",
+    MusicOffSelected_png : "res/menu/music-on-selected.png",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
