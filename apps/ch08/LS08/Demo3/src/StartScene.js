df2e48a1634e39e40f0599a3341310b176d47484
wenqiu.he <wenqiu.he@yahoo.com>
2018-08-09 15:40:04 +0800

    add demo

---
+++ apps/ch08/LS08/Demo3/src/StartScene.js
@@ -0,0 +1,35 @@
+var CustomLayer = cc.Layer.extend({
+	ctor: function () {
+		this._super();
+		var size = cc.winSize;
+		var label = new cc.LabelTTF('I am CustomLayer', '', 50);
+		label.setPosition(size.width / 2, size.height / 2);
+		this.addChild(label);
+		return true;
+	}
+});
+
+var BgLayer = cc.Layer.extend({
+	ctor: function () {
+		this._super();
+		var gdLayer1 = new cc.LayerGradient(cc.color.RED, new cc.Color(255, 0, 0, 0), cc.p(0, -1));
+		var gdLayer2 = new cc.LayerGradient(cc.color.RED, new cc.Color(255, 0, 0, 0), cc.p(-1, -1),
+			[{p: 0, color: cc.color.YELLOW},
+			 {p: .5, color: new cc.Color(0, 0, 0, 0)},
+			 {p: 1, color: cc.color.BLUE}]);
+
+		// this.addChild(gdLayer1);
+		this.addChild(gdLayer2);
+	}
+});
+
+var StartScene = cc.Scene.extend({
+	ctor: function () {
+		this._super();
+		var bgLayer = new BgLayer();
+		this.addChild(bgLayer);
+
+		var customLayer = new CustomLayer();
+		this.addChild(customLayer);
+	}
+});
