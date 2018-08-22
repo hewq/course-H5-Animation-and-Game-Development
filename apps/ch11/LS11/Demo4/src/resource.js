73d0e245c70b5b1cc89c184353e821d24a3cba3c
wenqiu.he <wenqiu.he@yahoo.com>
2018-08-22 18:07:40 +0800

    add demo3

---
+++ apps/ch11/LS11/Demo4/src/resource.js
@@ -0,0 +1,13 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+
+    Run_Png : "res/Run.png",
+    Run_Plist : "res/Run.plist",
+    Stone_Png : "res/stone.png",
+    Land_Png : "res/land.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
