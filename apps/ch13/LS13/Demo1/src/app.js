c9c4efd7961cca992598c12d2c68c5e3894ac729
wenqiu.he <wenqiu.he@yahoo.com>
2018-08-28 14:29:21 +0800

    add Demo2

---
+++ apps/ch13/LS13/Demo1/src/app.js
@@ -0,0 +1,62 @@
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        var size = cc.winSize;
+
+        // 系统自带粒子效果
+        // var particle = new cc.ParticleFire();
+        // var particle = new cc.ParticleExplosion();
+        // var particle = new cc.ParticleFireworks();
+        // var particle = new cc.ParticleFlower();
+        var particle = new cc.ParticleGalaxy();
+        // var particle = new cc.ParticleMeteor();
+        // var particle = new cc.ParticleRain();
+        // var particle = new cc.ParticleSmoke();
+        // var particle = new cc.ParticleShow();
+        // var particle = new cc.ParticleSpiral();
+        // var particle = new cc.ParticleSun();
+
+        particle.texture = cc.textureCache.addImage(res.Fire_png);
+        particle.x = size.width / 2;
+        particle.y = size.height / 2;
+        this.addChild(particle);
+        
+        // 自定义粒子效果
+        // var customParticle = new cc.ParticleSystem(res.Untitled_plist);
+        // customParticle.x = size.width / 5;
+        // customParticle.y = size.height / 2;
+        // this.addChild(customParticle);
+
+        // customParticle.setTotalParticles(500);
+        // customParticle.setStartSize(20);
+        // customParticle.setStartSizeVar(5);
+        // customParticle.setEndSize(20);
+        // customParticle.setEndSizeVar(5);
+
+        // customParticle.setEmitterMode(cc.ParticleSystem.MODE_RADIUS);
+        // customParticle.startRadius = 0;
+        // customParticle.endRadius = 100;
+        // customParticle.rotatePerS = 100;
+        // customParticle.rotatePerSVar = 50;
+        // customParticle.setEndSize(10);
+
+        // customParticle.setLife(2);
+        // customParticle.setStartColor(cc.color.RED);
+        // customParticle.setEndColor(cc.color.YELLOW);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
---
+++ apps/ch13/LS13/Demo1/src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+
+    Untitled_plist : "res/Untitled.plist",
+    Untitled_png : "res/Untitled.png",
+    Fire_png : "res/fire.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
